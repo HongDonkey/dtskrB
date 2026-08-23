9031e0ddea37b45d8df447f31dd132c3d149c4e5
HongDonkey <hdk2006@gmail.com>
2026-08-24 08:20:52 +0900

    베리얼묘티스몬, 베놈묘티스몬 이미지 수정

---
+++ src/main/resources/db/migration/V23__fix_myotismon_images.sql
@@ -0,0 +1,29 @@
+UPDATE digimon_kr
+SET image_url = '/digimon/portraits/ui_chara_icon_1600.webp',
+    pixel_image_url = '/digimon/pixels/dot600.gif'
+WHERE id = 233;
+
+UPDATE digimon_kr
+SET image_url = '/digimon/portraits/ui_chara_icon_1233.webp',
+    pixel_image_url = '/digimon/pixels/dot233.gif'
+WHERE id = 600;
+
+UPDATE digimon_en
+SET image_url = '/digimon/portraits/ui_chara_icon_1600.webp',
+    pixel_image_url = '/digimon/pixels/dot600.gif'
+WHERE id = 233;
+
+UPDATE digimon_en
+SET image_url = '/digimon/portraits/ui_chara_icon_1233.webp',
+    pixel_image_url = '/digimon/pixels/dot233.gif'
+WHERE id = 600;
+
+UPDATE digimon_jp
+SET image_url = '/digimon/portraits/ui_chara_icon_1600.webp',
+    pixel_image_url = '/digimon/pixels/dot600.gif'
+WHERE id = 233;
+
+UPDATE digimon_jp
+SET image_url = '/digimon/portraits/ui_chara_icon_1233.webp',
+    pixel_image_url = '/digimon/pixels/dot233.gif'
+WHERE id = 600;
